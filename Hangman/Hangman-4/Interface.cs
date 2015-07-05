0fca67b41cfc8356fcb15f0904e25e0f9a83595c
Martin Krustev <mr.krustev1994@gmail.com>
2015-07-05 17:34:38 +0300

    Add access modifiers.

--- Hangman/Hangman-4/Interface.cs
+++ Hangman/Hangman-4/Interface.cs
@@ -7,9 +7,9 @@
 
     public class Interface
     {
-        Random random = new Random();
+        private Random random = new Random();
 
-        static void Main()
+        private static void Main()
         {
             Hangman game = new Hangman();
             Console.WriteLine("Welcome to Hangman");
@@ -62,14 +62,13 @@
                     }
                 }
 
-                Console.WriteLine("Use 'top' to view the top scoreboard," +
-                    "'restart' to start a new game, \n'help' to cheat and 'exit' to quit the game.");
+                Console.WriteLine("Use 'top' to view the top scoreboard," + "'restart' to start a new game, \n'help' to cheat and 'exit' to quit the game.");
                 game.PrintWord();
                 Console.Write("enter a letter or command: ");
                 input = Console.ReadLine();
             }
 
-            Console.WriteLine("Goodby");
+            Console.WriteLine("Goodbye");
         }
     }
 }
