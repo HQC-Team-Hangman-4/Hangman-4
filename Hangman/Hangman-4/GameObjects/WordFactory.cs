b805679629695c9645a8827627721d2d30de9448
mr.krustev <mr.krustev1994@gmail.com>
2015-09-22 19:42:01 +0300

    Added WordFactory logic for word creation.

---
+++ Hangman/Hangman-4/GameObjects/WordFactory.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Collections.Generic;
+
+namespace HangMan.GameObjects
+{
+    using System;
+    using System.Linq;
+
+    using HangMan.InputProviders;
+    using HangMan.Interfaces;
+
+    public class WordFactory
+    {
+        private Random random = new Random();
+
+        public WordFactory()
+        {
+        }
+
+        public IWord GetWord(Categories category)
+        {
+            switch (category)
+            {
+                case Categories.Astronomy:
+                    return new Word(WordAsLetters(Categories.Astronomy));
+                case Categories.Biology:
+                    return new Word(WordAsLetters(Categories.Biology));
+                case Categories.Geography:
+                    return new Word(WordAsLetters(Categories.Geography));
+                case Categories.IT:
+                    return new Word(WordAsLetters(Categories.IT));
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        // Helper methods
+        private string WordFromFile(Categories category)
+        {
+            var allWords = DataSerialization.ReadFromFile(FileNames.words)
+                                            .Where(x => x.Contains(category.ToString())).ToArray();
+
+            var randomIndex = random.Next(0, allWords.Count());
+            var separator = allWords[randomIndex].IndexOf(" ", StringComparison.Ordinal) + 1;
+            var word = allWords[randomIndex].Substring(separator);
+
+            return word;
+        }
+
+        private IList<ILetter> WordAsLetters(Categories category)
+        {
+            var allLetters = new List<ILetter>();
+            var word = WordFromFile(category);
+
+            foreach (var letter in word)
+            {
+                allLetters.Add(new Letter(letter.ToString()));
+            }
+
+            return allLetters;
+        }
+    }
+}
---
+++ Hangman/Testing/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Text;
+using System.Threading.Tasks;
+using HangMan.GameObjects;
+using HangMan.InputProviders;
+using HangMan.Interfaces;
+
+namespace Testing
+{
+
+    class Program
+    {
+        private static Random random = new Random();
+
+        static void Main()
+        {
+            var allWords = DataSerialization.ReadFromFile(FileNames.words)
+                                            .Where(x => x.Contains("IT")).ToArray();
+
+            var randomIndex = random.Next(0, allWords.Count());
+            var separator = allWords[randomIndex].IndexOf(" ") + 1;
+            var word = allWords[randomIndex].Substring(separator);
+
+            var allLetters = new List<ILetter>();
+
+            foreach (var letter in word)
+            {
+                allLetters.Add(new Letter(letter.ToString()));
+            }
+
+            foreach (var letter in allLetters)
+            {
+                Console.WriteLine(letter.Value);
+            }
+        }
+    }
+}
