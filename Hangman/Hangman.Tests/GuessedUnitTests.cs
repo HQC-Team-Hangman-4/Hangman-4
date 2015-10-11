2f37401a4560d85463c7e894f48e724fc0711aeb
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-10-11 23:12:46 +0300

    Added Guessed tests

---
+++ Hangman/Hangman.Tests/GuessedUnitTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Hangman.Tests
+{
+    using System;
+    using System.IO;
+    using HangMan.Interfaces;
+    using HangMan.Renderers;
+    using NUnit.Framework;
+    using Telerik.JustMock;
+    using Hangman.Tests.Mocks;
+    using System.Collections.Generic;
+    using HangMan.GameObjects;
+
+    [TestFixture]
+    public class GuessedUnitTests
+    {
+        [TestCase]
+        public void GuessedShouldNotBeNullWhenInstantiated()
+        {
+            var renderableMock = Mock.Create<IRenderable>();
+            var guessed = new Guessed(renderableMock);
+
+            Assert.IsNotNull(guessed);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RenderableShouldNotBeNullWhenGuessedInstantiated()
+        {
+            IRenderable renderable = null;
+            var guessed = new Guessed(renderable);
+        }
+
+        [TestCase]
+        public void GetBodyShouldReturnString()
+        {
+            var renderableMock = Mock.Create<IRenderable>();
+            var guessed = new Guessed(renderableMock);
+
+            Assert.That(guessed.GetBody(), Is.TypeOf(typeof(string)));
+        }
+    }
+}
