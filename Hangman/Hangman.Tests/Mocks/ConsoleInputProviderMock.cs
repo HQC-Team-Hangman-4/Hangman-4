bbe5c0a84d87557ecff2306899a66b2dfc45dfc9
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-10-11 16:29:01 +0300

    Added ConsoleInputProviderMock

---
+++ Hangman/Hangman.Tests/Mocks/ConsoleInputProviderMock.cs
@@ -0,0 +1,26 @@
+﻿namespace Hangman.Tests.Mocks
+{
+    using HangMan.Interfaces;
+
+    public class ConsoleInputProviderMock : IInputProvider
+    {
+        private string command;
+
+         public string Command
+         {
+             get
+             {
+                 return this.command;
+             }
+             set 
+             {
+                 this.command = value;
+             }           
+         }
+
+         public void GetInput()
+         {
+             this.Command = "string_134";
+         }
+    }
+}
