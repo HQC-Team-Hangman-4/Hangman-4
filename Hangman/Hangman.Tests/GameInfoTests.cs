e10bc2929dcd437767cb82d18a558490f6c3bae0
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-10-11 23:12:19 +0300

    Added GameInfo tests

---
+++ Hangman/Hangman.Tests/GameInfoTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Hangman.Tests
+{
+    using HangMan.GameObjects;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GameInfoTests
+    {
+        [TestCase]
+        public void GameInfoShouldNotBeNullWhenInstantiated()
+        {
+            var gameInfo = new GameInfo();
+
+            Assert.IsNotNull(gameInfo);
+        }
+
+        [TestCase]
+        public void MistakesShouldNBeZeroWhenGameInfoCreated()
+        {
+            var gameInfo = new GameInfo();
+
+            int expected = 0;
+            int actual = gameInfo.Mistakes;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase]
+        public void UsedLettersShouldNBeZeroWhenGameInfoCreated()
+        {
+            var gameInfo = new GameInfo();
+
+            int expected = 0;
+            int actual = gameInfo.UsedLetters.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
