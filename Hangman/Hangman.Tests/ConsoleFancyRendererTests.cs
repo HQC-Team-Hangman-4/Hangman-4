44d443613add7c3ffe6902fc1d8c91caa39c0a3e
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-10-11 19:26:03 +0300

    Added COnsoleFancyRenderer test class, unfinished

---
+++ Hangman/Hangman.Tests/ConsoleFancyRendererTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Hangman.Tests
+{
+    using System;
+    using System.IO;
+    using HangMan.Interfaces;
+    using HangMan.Renderers;
+    using NUnit.Framework;
+    using Telerik.JustMock;
+    using Hangman.Tests.Mocks;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class ConsoleFancyRendererTests : ConsoleRendererTestsSetup
+    {
+        [TestCase]
+        public void ConsoleFancyRendererShouldNotBeNullWhenInstantiated()
+        {
+            var consolefancyRenderer = new ConsoleFancyRenderer();
+
+            Assert.IsNotNull(consolefancyRenderer);
+        }
+
+        [TestCase]
+        public void PrintInitialScreenShouldPrintToConsole() 
+        {
+            var consolefancyRenderer = new ConsoleFancyRenderer();
+            consolefancyRenderer.PrintInitialScreen();
+            this.textWriter.Close();
+
+            string expected = "Used Letters --> " + Environment.NewLine;
+            string actual = File.ReadAllText("./consolewritertests.txt");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
